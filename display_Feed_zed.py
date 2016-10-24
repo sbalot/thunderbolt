28a13a0af93ce5303bd188c0a4186e7a4bb6f551
Madhumitha <madhusvel@gmail.com>
2016-10-24 17:13:05 +0530

    Add files via upload
    Python code for displaying live feed from ZED camera.

---
+++ display_Feed_zed.py
@@ -0,0 +1,33 @@
+# This program captures live feed from Zed camera and displays the feed as
+# images after saving them
+#
+#
+#
+# Created by Madhumitha Senthilvel, 29 Sept 2016
+#
+#Reference: https://www.youtube.com/watch?v=v30XjzzeAS4
+
+import cv2
+import numpy as np
+
+capture= cv2.VideoCapture(0)                    #0 for webcam. 
+print('Press Esc key for exit')
+#open loop while cam is connected
+while (capture.isOpened()):
+    #BGR image feed from camera
+    ret,img=capture.read()
+    cv2.imshow('output',img)                    #display BGR image captured
+    #BGR to grayscale conversion
+    img2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)   #Convert color to gray scale
+    cv2.imshow('black & white',img2)            #display grayscale image
+    
+    
+    k=cv2.waitKey(10)                           # Press esc for exit
+    if k==27:
+        break
+    
+capture.release()
+cv2.destroyAllWindows()
+
+
+
